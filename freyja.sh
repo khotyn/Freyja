882b4199f51b46e9038e05bc8fb1d366386db3ce
khotyn <hting1@gmail.com>
2012-03-08 20:21:44 +0800

    update freyja.sh

--- freyja.sh
+++ freyja.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 fileName=`basename "$1" | sed 's/ /_/g'`
+newFile="/Users/apple/script/Freyja/$fileName"
+mv "$1" "$newFile"
 cd "/Users/apple/script/Freyja"
-touch "$fileName"
-cat "$1" > "$fileName"
 git add $fileName
-git commit -m 'Upload picture $fileName'
+git commit -m "Upload picture $fileName"
 git push -u origin master
 echo "https://github.com/khotyn/Freyja/raw/master/$fileName"
